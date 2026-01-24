0d7d6d10bf18c69127be9d0ea255f5572debbbb7
surajktdev <65347679+surajktdev@users.noreply.github.com>
2026-01-25 01:11:23 +0530

    Update User.java

--- user-service/src/main/java/com/rentflex/userservice/model/User.java
+++ user-service/src/main/java/com/rentflex/userservice/model/User.java
@@ -16,7 +16,7 @@ public class User {
     @Column(name = "user_name", nullable = false)
     private String userName;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)
