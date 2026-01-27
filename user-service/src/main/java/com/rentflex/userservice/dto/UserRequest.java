bcf0a25c728771ebd75133dabc1919e323aff68d
surajktdev <65347679+surajktdev@users.noreply.github.com>
2026-01-28 00:41:52 +0530

    code cleanup

--- user-service/src/main/java/com/rentflex/userservice/dto/UserRequest.java
+++ user-service/src/main/java/com/rentflex/userservice/dto/UserRequest.java
@@ -6,15 +6,13 @@ import jakarta.validation.constraints.*;
 
 public record UserRequest(
         @JsonProperty("userName")
-        @NotBlank(message = "Username is mandatory")
-        @Size(min = 3, message = "Username must be at least 3 characters long")
-        String userName,
-
+                @NotBlank(message = "Username is mandatory")
+                @Size(min = 3, message = "Username must be at least 3 characters long")
+                String userName,
         @NotBlank(message = "Email is required")
-        @Email(message = "Please provide a valid email address")
-        String email,
+                @Email(message = "Please provide a valid email address")
+                String email,
         @NotBlank(message = "Password is required")
-        @Size(min = 3, message = "Password must be at least 8 characters long")
-        String password,
-        @NotNull(message = "Role is mandatory") Role role) {
-}
+                @Size(min = 3, message = "Password must be at least 8 characters long")
+                String password,
+        @NotNull(message = "Role is mandatory") Role role) {}
--- user-service/src/main/java/com/rentflex/userservice/exception/GlobalExceptionHandler.java
+++ user-service/src/main/java/com/rentflex/userservice/exception/GlobalExceptionHandler.java
@@ -18,9 +18,12 @@ public class GlobalExceptionHandler {
 
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(
+                        error -> {
+                            errors.put(error.getField(), error.getDefaultMessage());
+                        });
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
