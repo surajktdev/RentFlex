fa34564a25841287ce121f1fcfc1209216ae1ee8
surajktdev <65347679+surajktdev@users.noreply.github.com>
2026-01-28 00:41:09 +0530

    handled ValidationErrors

--- user-service/src/main/java/com/rentflex/userservice/dto/UserRequest.java
+++ user-service/src/main/java/com/rentflex/userservice/dto/UserRequest.java
@@ -5,14 +5,16 @@ import com.rentflex.userservice.model.Role;
 import jakarta.validation.constraints.*;
 
 public record UserRequest(
-        @JsonProperty("name")
-                @NotBlank(message = "Username is mandatory")
-                @Size(min = 3, message = "Username must be at least 3 characters long")
-                String userName,
+        @JsonProperty("userName")
+        @NotBlank(message = "Username is mandatory")
+        @Size(min = 3, message = "Username must be at least 3 characters long")
+        String userName,
+
         @NotBlank(message = "Email is required")
-                @Email(message = "Please provide a valid email address")
-                String email,
+        @Email(message = "Please provide a valid email address")
+        String email,
         @NotBlank(message = "Password is required")
-                @Size(min = 8, message = "Password must be at least 8 characters long")
-                String password,
-        @NotNull(message = "Role is mandatory") Role role) {}
+        @Size(min = 3, message = "Password must be at least 8 characters long")
+        String password,
+        @NotNull(message = "Role is mandatory") Role role) {
+}
